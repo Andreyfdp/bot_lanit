ab3f010061289037d70753c6628d61443e4d6c45
Andreyfdp <ilinandrej738@gmail.ry>
2023-08-23 14:43:31 +0300

    handlers files

--- handlers/photo.py
+++ handlers/photo.py
@@ -3,4 +3,4 @@ from aiogram import Router, types,F
 router = Router()
 @router.message(F.photo)
 async def process_photo(message: types.Message):
-    await message.answer_photo(message.photo[0].file_id)
+    await message.answer_photo(message.photo[-1].file_id)
